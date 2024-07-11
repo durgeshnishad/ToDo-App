81544b33db6493868055f3a25ec96c8c68f9abf1
Durgesh Nishad <dnishad042@gmail.com>
2024-07-11 15:20:35 +0530

    added fontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+
+export function CreateTodo(){
+     return <div>
+        <input type="text" placeholder="title"></input>
+        <input type="text" placeholder="description"></input>
+
+        <button>Add Todo</button>
+     </div>
+}
