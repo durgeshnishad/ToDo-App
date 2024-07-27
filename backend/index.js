0cea6fad3cedbaa8bbe6ae41f61eeac546430ad9
Durgesh Nishad <dnishad042@gmail.com>
2024-07-27 13:40:05 +0530

    fronten+backend commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express= require('express')
+const cors= require('cors')
 const {createTodo}= require('./types.js')
 const { todo } = require('./db.js')
 const app= express()
 const port=3000
 
-
-
 app.use(express.json())
+app.use(cors())
 
 app.get('/todos',async function(req, res){
     const todos= await todo.find({})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState([])
+  
+  fetch ("http://localhost:3000/todos")
+    .then(async function(res){
+      const json = await res.json()
+      setTodos(json.todos)
+    })
+  
   return (
     <div>
+      <CreateTodo></CreateTodo>
+      <Todos todos={todos}></Todos>
     </div>
   )
 }
